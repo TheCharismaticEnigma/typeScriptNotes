67bc200857584acab3a33fd0295ab04ba82d397e
TheCharismaticEnigma <avishkar2002@mail.com>
2023-01-28 18:35:33 +0530

    Section Notes: Advanced Types

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,27 @@
 "use strict";
-let Person = {
-    name: "Enimga",
-    birthYear: 1993,
-    age: function () {
-        return 2023 - this.birthYear;
+const firstEmployee = {
+    id: 1212121,
+    name: "Uncooked Sausage",
+    tenure: 25,
+    canRetire: function (tenure) {
+        tenure = tenure || this.tenure;
+        return tenure > 5;
     },
 };
-console.log(Person.age());
+const secondEmployee = {
+    id: 121212121,
+    name: "Skinny Twerp",
+    tenure: 15,
+    canRetire: function (tenure) {
+        tenure = tenure || this.tenure;
+        return tenure > 10;
+    },
+};
+function kgToLbs(weight) {
+    if (typeof weight === "number")
+        return Math.trunc(weight * 2.2);
+    return parseInt(weight) * 2.2;
+}
+console.log(kgToLbs(100));
+console.log(kgToLbs("10"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,81 @@
-const School: { 
-    name: string, 
-    readonly foundingYear: number, 
-    readonly totalStudents: number 
-    studentsPerHouse: (totalStudents: number) => number ; 
-} = { 
-    name: 'SSS' , 
-    foundingYear: 1990 , 
-    totalStudents: 600, 
-    studentsPerHouse: function(houses) { 
-        return this.totalStudents / houses + ; 
-    }
+/*
+const School: {
+  name: string;
+  readonly foundingYear: number;
+  readonly totalStudents: number;
+  studentsPerHouse: (totalStudents: number) => number;
+} = {
+  name: "SSS",
+  foundingYear: 1990,
+  totalStudents: 600,
+  studentsPerHouse: function (houses) {
+    return this.totalStudents / houses;
+  },
+};
+
+// Instead of Declaring three constants
+
+const enum PersonTraits {
+  Name = "Avi",
+  Age = 10,
+  Gender = "Tranny",
 }
+
+const theName: PersonTraits = PersonTraits.Name; 
+
+*/
+
+/* TOPIC: Type Aliases: 
+
+const Person: {
+  name: string;
+  age: number;
+  sex: string;
+} = {
+  name: `Avi`,
+  age: 15,
+  sex: "M",
+};
+
+*/
+
+type Employee = {
+  readonly id: number;
+  name: string;
+  tenure: number;
+  canRetire: (tenure?: number) => boolean;
+};
+
+const firstEmployee: Employee = {
+  id: 1212121,
+  name: "Uncooked Sausage",
+  tenure: 25,
+  canRetire: function (tenure?: number) {
+    tenure = tenure || this.tenure;
+    return tenure > 5;
+  },
+};
+
+const secondEmployee: Employee = {
+  id: 121212121,
+  name: "Skinny Twerp",
+  tenure: 15,
+  canRetire: function (tenure?: number) {
+    tenure = tenure || this.tenure; // if falsy then this.tenure
+    return tenure > 10;
+  },
+};
+
+// console.log(firstEmployee.name);
+// console.log(secondEmployee.name);
+
+function kgToLbs(weight: number | string): number {
+  // Narrowing
+
+  if (typeof weight === "number") return Math.trunc(weight * 2.2);
+
+  return parseInt(weight) * 2.2; // here data type is string
+}
+
+console.log(kgToLbs(100));
+console.log(kgToLbs("10"));
